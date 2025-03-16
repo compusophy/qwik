6ef1f49dfa4fc8e823d21e755e172b673a4023df
Varixo <admin@varixo.pl>
2025-03-16 19:17:26 +0100

    fix: change client side generated ID to start with build base and add underscore if it is starting from number

--- packages/qwik/src/core/use/use-id.ts
+++ packages/qwik/src/core/use/use-id.ts
@@ -1,8 +1,6 @@
 import type { QRL } from '..';
 import { hashCode } from '../shared/utils/hash_code';
 import { OnRenderProp } from '../shared/utils/markers';
-import { isDomContainer } from '../client/dom-container';
-import type { SSRContainer } from '../ssr/ssr-types';
 import { useSequentialScope } from './use-sequential-scope';
 import { getNextUniqueIndex } from '../shared/utils/unique-index-generator';
 
@@ -12,13 +10,16 @@ export const useId = (): string => {
   if (val != null) {
     return val;
   }
-  const containerBase = isDomContainer(iCtx.$container$)
-    ? ''
-    : (iCtx.$container$ as SSRContainer).buildBase || '';
+  const containerBase = iCtx.$container$.$buildBase$ || '';
   const base = containerBase ? hashCode(containerBase) : '';
   const componentQrl = iCtx.$container$.getHostProp<QRL>(iCtx.$hostElement$, OnRenderProp);
   const hash = componentQrl?.getHash() || '';
   const counter = getNextUniqueIndex(iCtx.$container$) || '';
-  const id = `${base}-${hash}-${counter}`; // If no base and no hash, then "--#"
+  let id = `${base}-${hash}-${counter}`;
+
+  // add `_` if starts with a number, because CSS does not allow class names to start with a number
+  if (id.charCodeAt(0) >= 48 /* 0 */ && id.charCodeAt(0) <= 57 /* 9 */) {
+    id = `_${id}`;
+  }
   return set(id);
 };
